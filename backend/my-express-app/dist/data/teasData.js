8dbd997984f55c8bff62e0296bee86cb1bf459e8
Kate <belova.ek0@gmail.com>
2025-07-15 14:59:19 +0500

    Удаление работы со статическими данными из файлов

--- backend/my-express-app/dist/data/teasData.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.getTeas = getTeas;
-const teasDataFs_1 = require("./fs/teasDataFs");
-function getTeas() {
-    return teasDataFs_1.teaProducts;
-}
